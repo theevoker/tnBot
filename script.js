b5e5d6f3f436c8e695976626fad6c43864a0232c
DavidEl-Natan <111539128+DavidEl-Natan@users.noreply.github.com>
2022-08-19 14:04:27 +0300

    trying

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
+function randomChoice(arr) {
+    return arr[Math.floor(arr.length * Math.random())];
+}
+let start = false;
+let book = 'בראשית'
+let ans = true
+let pasuk = ''
+function ask(){
+    book = randomChoice(Object.keys(PSUKIM_BAHOMER));
+    pasuk=randomChoice(PSUKIM_BAHOMER[book].split(":"));
+    send(pasuk);
+}
+function correct(book){
+    send('נכון מאוד! הפסוק נמצא בספר ' + book);
+    ask();
+}
+function incorrect(text){
+    send('לא נכון. הפסוק לא נמצא בספר ' + text)
+}
+function check(text){
+    if(!start){
+        start=true;
+        ask();
+        return;
+    }
+    else{
+        ans = book === text;
+    }
+    if(ans){
+        correct(book);
+    }
+    else{
+        incorrect(text);
+    }
+}
+
 function submitMy(){
     if(document.getElementById("myInput").value!==""){
+        var txt = document.getElementById("myInput").value
         let message=document.createElement("p");
-        message.setAttribute("class", "myMessage")
-        let text=document.createTextNode(document.getElementById("myInput").value);
+        message.setAttribute("class", "myMessage");
+        let text=document.createTextNode(txt);
         document.getElementById("myInput").value='';
         message.appendChild(text);
         document.getElementById("back").appendChild(message);
         let screen = document.getElementById("back");
         screen.scrollTop = screen.scrollHeight;
+        check(txt);
     }
 }
 function submitHis() {
@@ -23,9 +61,11 @@ function submitHis() {
     }
 }
 function send(message){
-    let mess = document.createElement("p");
-    mess.setAttribute("class", "hisMessage");
+    let messa = document.createElement("p");
+    messa.setAttribute("class", "hisMessage");
     let text = document.createTextNode(message);
-    mess.appendChild(text);
-    document.getElementById("back").appendChild(mess);
+    messa.appendChild(text);
+    document.getElementById("back").appendChild(messa);
+    let screen = document.getElementById("back");
+    screen.scrollTop = screen.scrollHeight;
 }
